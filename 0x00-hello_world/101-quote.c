4874c4455ccfd798dfbcac0b1b48c09e47d2fc66
ebbyjerop <ebbyjerop500@gmail.com>
2022-06-30 17:28:58 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,13 @@
 #include <unistd.h>
 
 /**
- * main-prints exactly"and that piece of act is useful"
+ * main-prints exactly"and that piece of art is useful"
  * - Ebby Jerop, 2022-07-01",
  * followed by a new line, to the standad error.
  * Return: 1 if success.
  */
 int main(void)
 {
-	write(2, "and that piece of act is useful\" - Dora korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
