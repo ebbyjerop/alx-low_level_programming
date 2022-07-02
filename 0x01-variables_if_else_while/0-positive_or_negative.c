de236050453abb728177cd1ba9fa93e883254021
ebbyjerop <ebbyjerop500@gmail.com>
2022-07-02 09:15:51 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,25 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ * main -prints negative or positive
+ * description: using the main function to print positive or negative number
+ * Return: 0
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	if (n > 0)
+		printf("%d is positive\n", n)
+	if (n == 0)
+		printf("%d is zero\n", n)
+	if (n < 0)
+	{
+		printf("%d is negative\n", n)
+	}
 	return (0);
 }
