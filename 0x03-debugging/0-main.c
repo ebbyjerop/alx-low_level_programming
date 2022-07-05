b4234d2c2a8abba9a5db2a7297ea7c944e1c7337
ebbyjerop <ebbyjerop500@gmail.com>
2022-07-05 15:05:29 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,20 +5,32 @@
  * positive_or_negative - main block
  * @i: Get a random number and print the number
  * and if it is positive, negative, or zero
- * Return: i
+ * Return: 0
  */
 void positive_or_negative(int i)
 {
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	n = i;
 	if (n > 0)
 		printf("%i is positive\n", n);
 	else if (n < 0)
 		printf("%i is negative\n", n);
 	else
 		printf("%i is zero\n", n);
+	return (0);
+}
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+int main(void)
+{
+	int i;
 
-	return (i);
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
 }
